e1b2db1250433dfec55d7492cb4197ba6797d75b
david-nunoo-afrane <163885636+david-nunoo-afrane@users.noreply.github.com>
2024-11-19 14:43:56 +0000

    Create odd-and-even.py

---
+++ odd-and-even.py
@@ -0,0 +1,8 @@
+# Code to check if a number is even or odd.
+# Enter a number, and the program tells if it's "Even" or "Odd."
+
+num = int(input("Enter a number: "))  
+if num % 2 == 0: 
+    print("Even") .
+else:  
+    print("Odd") 
