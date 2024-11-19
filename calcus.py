fe0da898474b698c520b3ede8c1a96a1750b6de3
david-nunoo-afrane <163885636+david-nunoo-afrane@users.noreply.github.com>
2024-11-19 15:16:45 +0000

    Create calcus.py

---
+++ calcus.py
@@ -0,0 +1,18 @@
+# Code to accept two numbers and calculate using the receivved operator.
+
+first_number = int(input("Enter first number: "))
+
+second_number = int(input("Enter second number: "))
+
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == "+":
+    print(first_number + second_number)
+elif operator == "-":
+    print(first_number - second_number)
+elif operator == "*":
+    print(first_number * second_number)
+elif operator == "/":
+    print(first_number / second_number)
+else:
+    print("Invalid operator")
